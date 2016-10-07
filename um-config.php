6fcc609f86b809bd1860e603490ebfd2be732641
vovodroid <vovodroid@gmail.com>
2016-10-07 12:08:51 +0300

    Move BCC to admin option to the top

--- um-config.php
+++ um-config.php
@@ -569,6 +569,14 @@ $this->sections[] = array(
     'fields'     => array(
 
 		array(
+                'id'       => 'bcc_user_emails',
+                'type'     => 'switch',
+                'title'    => __( 'BCC user emails to Admin','ultimatemember' ),
+				'default'  => 0,
+				'desc' 	   => __('Whether to BCC all user emails to admin address set in Notifocation page.','ultimatemember'),
+        ),
+
+		array(
 				'id'       => 'mail_from',
                 'type'     => 'text',
                 'title'    => __( 'Mail appears from','ultimatemember' ),
@@ -590,14 +598,6 @@ $this->sections[] = array(
                 'title'    => __( 'Use HTML for E-mails?','ultimatemember' ),
 				'default'  => 0,
 				'desc' 	   => __('If you enable HTML for e-mails, you can customize the HTML e-mail templates found in <strong>templates/email</strong> folder.','ultimatemember'),
-        ),
-
-        array(
-                'id'       => 'bcc_user_emails',
-                'type'     => 'switch',
-                'title'    => __( 'BCC user emails to Admin','ultimatemember' ),
-				'default'  => 0,
-				'desc' 	   => __('Whether to BCC all user emails to admin address set in Notifocation page.','ultimatemember'),
         ),
 
         array(
